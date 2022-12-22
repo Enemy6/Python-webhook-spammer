1184879833b91a8fae62631fe08f9a7b1b3b07cf
ArabicCat <114680621+ArabicCat@users.noreply.github.com>
2022-12-22 13:25:38 +0100

    update v1.1

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from discord_webhook import DiscordWebhook
 from colorama import init, Fore, Style
 import requests
 
-print(Fore.BLUE + "ARABIC/JIMBOB'S WEBHOOK SPAMMER!!!!!!!!!  \n")
-
 init(autoreset=True)
 
 def is_valid_webhook(url):
@@ -14,26 +12,35 @@ def is_valid_webhook(url):
         return False
 
 while True:
+    print('\n\n                       ▄▀▄     ▄▀▄' + "\n"
+      '                      ▄█░░▀▀▀▀▀░░█▄' + "\n"
+      '                  ▄▄  █░░░░░░░░░░░█' + "\n"
+      '                 █▄▄█ █░░▀░░┬░░▀░░█ █▄▄█' + "\n"
+      "\n"
+      "                 Author: Kars#9142\n"
+      "                 GitHub: https://github.com/ArabicCat\n\n")
     url = input(Fore.GREEN + "Enter the url you wanna spam \n> ")
 
     if not is_valid_webhook(url):
         print(Fore.RED + "Error: Invalid webhook URL")
         continue
 
+    # Save the URL to the log file
+    with open("logs.txt", "a") as log_file:
+        log_file.write(url + "\n" + "\n")
+
     msg = input(Fore.GREEN + "\nEnter the message you wanna spam \n> ")
     amount = int(input(Fore.GREEN + "\nEnter the amount you wanna spam the message\n> "))
 
     webhook = DiscordWebhook(url=(url), rate_limit_retry=True,
-                            content=("@everyone" + (msg)))
+                            content=("@everyone " + (msg)))
     count = 1
     while count <= (amount):
         response = webhook.execute()
         print(Fore.YELLOW + f"Send message #{count}")
         count += 1
 
-    print(Fore.GREEN + "ez bitches")
-    print(Style.BRIGHT + Fore.GREEN + "\n\nThanks for using my epic programm :)")
-
-    again = input(Fore.BLUE + "Press ENTER to exetute the script again.\n\n")
+    again = input(Style.BRIGHT + Fore.GREEN + "\nPress ENTER to exetute the script again.\n\n")
     if again.lower() != "":
-        break
+        break 
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
