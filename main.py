ad800205cc65e3ac81aba6001fa0592c2fccab98
ArabicCat <114680621+ArabicCat@users.noreply.github.com>
2022-12-23 22:46:06 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from discord_webhook import DiscordWebhook
-from colorama import init, Fore, Style
 import requests
-
-init(autoreset=True)
+import os
+import time
 
 def is_valid_webhook(url):
     try:
@@ -12,21 +11,21 @@ def is_valid_webhook(url):
         return False
 
 while True:
-    print('\n\n                       ▄▀▄     ▄▀▄' + "\n"
+    print('\r\n\n                       ▄▀▄     ▄▀▄' + "\n"
       '                      ▄█░░▀▀▀▀▀░░█▄' + "\n"
       '                  ▄▄  █░░░░░░░░░░░█' + "\n"
       '                 █▄▄█ █░░▀░░┬░░▀░░█ █▄▄█' + "\n"
       "\n"
       "                 Author: Kars#9142\n"
       "                 GitHub: https://github.com/ArabicCat\n\n")
-    url = input(Fore.GREEN + "Enter the url you wanna spam (leave empty for default) \n> ")
+    url = input("Enter the url you wanna spam (leave empty for default) \n> ")
 
     if not is_valid_webhook(url):
-        print(Fore.RED + "Error: Invalid webhook URL")
+        print("Error: Invalid webhook URL")
         continue
 
-    msg = input(Fore.GREEN + "\nEnter the message you wanna spam (leave empty for default)\n> ")
-    amount = int(input(Fore.GREEN + "\nEnter the amount you wanna spam the message (25+ = RATELIMMITED)\n> "))
+    msg = input("\nEnter the message you wanna spam (leave empty for default)\n> ")
+    amount = int(input("\nEnter the amount you wanna spam the message (25+ = RATELIMMITED)\n> "))
 
     # If the user doesn't provide a message, use the default message
     if not msg:
@@ -37,14 +36,15 @@ while True:
     count = 1
     while count <= (amount):
         response = webhook.execute()
-        print(Fore.YELLOW + f"Send message #{count}")
+        print(f"Send message #{count}")
         count += 1
+        time.sleep(0.1)
 
     # Log the number of messages sent to the webhook
     with open("logs.txt", "a") as log_file:
-        log_file.write(f"{amount} Messages sent to: {url}\n" + "\n")
+        log_file.write(f"{amount} Messages sent to: {url}\n")
 
-    again = input(Style.BRIGHT + Fore.GREEN + "\nPress ENTER to exetute the script again.\n\n")
+    again = input("\nPress ENTER to exetute the script again.")
     if again.lower() != "":
         break 
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    os.system('cls' if os.name == 'nt' else 'clear')
