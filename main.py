65e409ce4db4fd0de5ecd445e838477ad11c76fb
ArabicCat <114680621+ArabicCat@users.noreply.github.com>
2022-12-24 18:29:04 +0100

    github being gay fr

--- main.py
+++ main.py
@@ -1,46 +1,22 @@
-from discord_webhook import DiscordWebhook
-import requests
-import os
-import time
+from colorama import Fore, Style, init
+import os 
 
-def is_valid_webhook(url):
-    try:
-        requests.post(url, json={'content': '@everyone discord.gg/DM8GtTT4rX'})
-        return True
-    except requests.exceptions.RequestException:
-        return False
+# Initialize colorama
+init()
 
-while True:
-    print('\r\n\n                       ▄▀▄     ▄▀▄' + "\n"
+print(f'{Fore.CYAN}\r\n\n                       ▄▀▄     ▄▀▄' + "\n"
       '                      ▄█░░▀▀▀▀▀░░█▄' + "\n"
       '                  ▄▄  █░░░░░░░░░░░█' + "\n"
       '                 █▄▄█ █░░▀░░┬░░▀░░█ █▄▄█' + "\n"
       "\n"
       "                 Author: Kars#9142\n"
-      "                 GitHub: https://github.com/ArabicCat\n\n")
-    url = input("Enter the url you wanna spam (leave empty for default) \n> ")
+      "                 GitHub: https://github.com/ArabicCat\n")
+option = input("Please select a option:\n1 Manual\n2 Auto\n> ")
 
-    if not is_valid_webhook(url):
-        print("Error: Invalid webhook URL")
-        continue
-
-    msg = input("\nEnter the message you wanna spam (leave empty for default)\n> ")
-    amount = int(input("\nEnter the amount you wanna spam the message (25+ = RATELIMMITED)\n> "))
-
-    # If the user doesn't provide a message, use the default message
-    if not msg:
-        msg = "@everyone hey babe, i 32 year old girl from canada look for sex. click my link and enter email for hot video: https://discord.gg/DM8GtTT4rX"
-
-    webhook = DiscordWebhook(url=(url), rate_limit_retry=True,
-                            content=((msg)))
-    count = 1
-    while count <= (amount):
-        response = webhook.execute()
-        print(f"Send message #{count}")
-        count += 1
-        time.sleep(0.1)
-
-    again = input("\nPress ENTER to exetute the script again.")
-    if again.lower() != "":
-        break 
-    os.system('cls' if os.name == 'nt' else 'clear')
+# Check the user's input and run the appropriate script
+if option == "1":
+    os.system('python manual.py')
+elif option == "2":
+    os.system('python auto.py')
+else:
+    print(Fore.RED + "Invalid option. Please chooce number 1 or 2.")
