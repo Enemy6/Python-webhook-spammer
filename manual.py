36cb368ac6fd88749838dabfbf447430c32c1831
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-12-24 12:55:08 -0800

    Update manual.py

--- manual.py
+++ manual.py
@@ -1,8 +1,27 @@
-from discord_webhook import DiscordWebhook
-from colorama import Fore, Style, init
-import requests
 import os
 import time
+
+import requests
+
+try:
+    from colorama import Fore
+except ImportError:
+    raise RuntimeError(
+        "The colorama module must be installed\nYou can install it with 'pip install colorama'"
+    )
+try:
+    from discord_webhook import DiscordWebhook
+except ImportError:
+    raise RuntimeError(
+        "The discord-webhook module must be installed\nYou can install it with 'pip install discord-webhook'"
+    )
+try:
+    import requests
+except ImportError:
+    raise RuntimeError(
+        "The requests module must be installed\nYou can install it with 'pip install requests'"
+    )
+
 
 def is_valid_webhook(url: str) -> bool:
     try:
@@ -16,38 +35,58 @@ def is_valid_webhook(url: str) -> bool:
     except requests.exceptions.RequestException:
         return False
 
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(f'{Fore.CYAN}\r\n\n                       ▄▀▄     ▄▀▄' + "\n"
-      '                      ▄█░░▀▀▀▀▀░░█▄' + "\n"
-      '                  ▄▄  █░░░░░░░░░░░█' + "\n"
-      '                 █▄▄█ █░░▀░░┬░░▀░░█ █▄▄█' + "\n"
-      "\n"
-      "                 Author: Kars#9142\n"
-      "                 GitHub: https://github.com/ArabicCat\n\n")
-    url = input(f"{Fore.GREEN}Enter the webhook URL you wanna spam (leave empty for default) \n> ")
 
-    if not is_valid_webhook(url):
-        print(f"{Fore.RED}Error: Invalid webhook URL")
-        continue
+def ask(msg, convertor):
+    try:
+        return convertor(input(msg))
+    except ValueError:
+        print(f"{Fore.RED}Invalid Input")
+        return ask(msg, convertor)
 
-    msg = input("\nEnter the message you wanna spam (leave empty for default)\n> ")
-    amount = int(input("\nEnter the amount you wanna spam the message (25+ = RATELIMMITED)\n> "))
 
-    # If the user doesn't provide a message, use the default message
-    if not msg:
-        msg = "@everyone it seems someone leaked your webhook url!!! https://discord.gg/DM8GtTT4rX"
+def main():
+    os.system("cls" if os.name == "nt" else "clear")
+    while True:
+        print(
+            f"{Fore.CYAN}\r\n\n                       ▄▀▄     ▄▀▄" + "\n"
+            "                      ▄█░░▀▀▀▀▀░░█▄" + "\n"
+            "                  ▄▄  █░░░░░░░░░░░█" + "\n"
+            "                 █▄▄█ █░░▀░░┬░░▀░░█ █▄▄█" + "\n"
+            "\n"
+            "                 Author: Kars#9142\n"
+            "                 GitHub: https://github.com/ArabicCat\n\n"
+        )
+        url = input(
+            f"{Fore.GREEN}Enter the webhook URL you wanna spam (leave empty for default) \n> "
+        )
 
-    webhook = DiscordWebhook(url=(url), rate_limit_retry=True,
-                            content=((msg)))
-    count = 1
-    while count <= (amount):
-        response = webhook.execute()
-        print(f"{Fore.YELLOW}Send message #{count}")
-        count += 1
-        time.sleep(0.1)
+        if not is_valid_webhook(url):
+            print(f"{Fore.RED}Error: Invalid webhook URL")
+            continue
 
-    again = input(f"{Fore.GREEN}\nPress ENTER to exetute the script again.")
-    if again.lower() != "":
-        break 
-    os.system('cls' if os.name == 'nt' else 'clear')
+        msg = input("\nEnter the message you wanna spam (leave empty for default)\n> ")
+        amount = ask(
+            "\nEnter the amount you wanna spam the message (25+ = RATELIMMITED)\n> ",
+            int,
+        )
+
+        # If the user doesn't provide a message, use the default message
+        if not msg:
+            msg = "@everyone it seems someone leaked your webhook url!!! https://discord.gg/DM8GtTT4rX"
+
+        webhook = DiscordWebhook(url=(url), rate_limit_retry=True, content=((msg)))
+        count = 1
+        while count <= (amount):
+            webhook.execute()
+            print(f"{Fore.YELLOW}Send message #{count}")
+            count += 1
+            time.sleep(0.1)
+
+        again = input(f"{Fore.GREEN}\nPress ENTER to exetute the script again.")
+        if again.lower() != "":
+            break
+        os.system("cls" if os.name == "nt" else "clear")
+
+
+if __name__ == "__main__":
+    main()
